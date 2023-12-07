5b583fdd07530992d7fd9e345063e66eeaa54c76
Adanene <128061161+Adanene@users.noreply.github.com>
2023-12-07 14:52:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -137,8 +137,8 @@ if ok:
     X_test = scaler.transform(X_test)
 
     model = Sequential()
-    model.add(Dense(64, input_dim=len(features), activation='relu'))
-    model.add(Dense(32, activation='relu'))
+    model.add(Dense(64, input_dim=len(features), activation='prelu'))
+    model.add(Dense(32, activation='prelu'))
     model.add(Dense(1, activation='linear'))  # Output layer for regression
 
     # Compile the model
